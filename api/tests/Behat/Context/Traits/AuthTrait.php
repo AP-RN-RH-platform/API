ca45a174b8a4a89c7e7f8b3067a4968a914df513
Soufian AIT TIRITE <contact@soufian-a.net>
2020-06-03 17:14:48 +0200

    Add tests for Users

--- api/tests/Behat/Context/Traits/AuthTrait.php
+++ api/tests/Behat/Context/Traits/AuthTrait.php
@@ -19,6 +19,42 @@ trait AuthTrait
     protected $authPassword;
 
     /**
+     * FeatureContext constructor.
+     *
+     * @param KernelInterface $kernel the kernel to get services.
+     */
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->manager = $kernel->getContainer()->get('doctrine.orm.default_entity_manager');
+        $this->jwtManager = $kernel->getContainer()->get('lexik_jwt_authentication.jwt_manager');
+    }
+
+    /**
+     * @BeforeScenario @loginAsAdmin
+     *
+     * @see https://symfony.com/doc/current/security/entity_provider.html#creating-your-first-user
+     *
+     * @param BeforeScenarioScope $scope the scope
+     */
+    public function loginAsAdmin(BeforeScenarioScope $scope)
+    {
+        //Test with a fake user
+        //$user = new User();
+        //$user->setEmail('admin@example.org');
+        //$user->setUsername('Admin');
+        //$user->setRoles(['ROLE_ADMIN']);
+        //Test with a user in database
+        /** @var UserRepository $userRepository */
+        $userRepository = $this->manager->getRepository(User::class);
+        /** @var User $user */
+        $user = $userRepository->findOneByEmail('recruiter@example.org');
+        $token = $this->jwtManager->create($user);
+        /** @var RestContext $restContext */
+        $this->restContext = $scope->getEnvironment()->getContext(RestContext::class);
+        $this->restContext->iAddHeaderEqualTo('Authorization', "Bearer $token");
+    }
+
+    /**
      * @Given /^I authenticate with user "([^"]*)" and password "([^"]*)"$/
      */
     public function iAuthenticateWithEmailAndPassword($email, $password)
--- api/tests/Behat/Feature/user.feature
+++ api/tests/Behat/Feature/user.feature
@@ -4,15 +4,24 @@ Feature: _User_
       | parameters     |
       | users          |
 
-  Scenario: test post user
+  Scenario: test create user
     Given I have the payload
     """
     {
-        email: "test@test.com"
-        password: "$argon2id$v=19$m=65536,t=4,p=1$cMH2swfacaqZCwGA5JWOPA$J09JznitkqUp0uGDsslacn63HhhNEikbhjbZtuYm4ns"
-        isValide: true
+        email: "test@test.com",
+        password: "$argon2id$v=19$m=65536,t=4,p=1$cMH2swfacaqZCwGA5JWOPA$J09JznitkqUp0uGDsslacn63HhhNEikbhjbZtuYm4ns",
+        firstname: "PLIP",
+        lastname: "PLIP",
+        genre: 1,
+        photo: "",
+        address: "3 Rue de la Paix",
+        city: "Paris",
+        roles: [
+          "ROLE_RECRUITER"
+        ]
     }
     """
     Given I request "POST /users"
     When the response status code should be 201
     Then print last response
+
